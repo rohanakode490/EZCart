9253f8935eb2d11d3be7c422b3cb0b45b7f67a93
Rohan <rohanakode52@gmail.com>
2023-10-15 15:44:27 +0530

    Error: If wrong id(short or long id) is given

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,10 +1,15 @@
 const ErrorHandler = require("../utils/errorHandler");
 
 module.exports = (err, req, res, next) => {
-
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
-  
+
+  // if wrong id / short id is given
+  if (err.name === "CastError") {
+    const message = `Resource not found.\n Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400)
+  }
+
   res.status(err.statusCode).json({
     success: false,
     error: err.message,
