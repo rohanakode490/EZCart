7bd8273c66516de767c9b3a66b25f8c103d5cf31
Rohan <rohanakode52@gmail.com>
2023-10-17 23:08:13 +0530

    Authentication middleware

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
 const errorMiddleware = require("./middleware/error");
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 const product = require("./routes/productRoute");
-const user = require("./routes/userRoute")
+const user = require("./routes/userRoute");
 
 app.use("/api/v1", product);
-app.use("/api/v1",user)
+app.use("/api/v1", user);
 
 // middleware for error
 app.use(errorMiddleware);
---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+const User = require("../models/user");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncError = require("./catchAsyncError");
+const jwt = require("jsonwebtoken");
+
+exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return next(new ErrorHandler("Please login to access this resource", 401));
+  }
+
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = await User.findById(decodedData._id); //from this we can use the user data from the request(req) when it is logged in
+
+  next();
+});
