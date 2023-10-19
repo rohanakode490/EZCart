b57fcefa100fddbe77d908969c8290ee447a41dd
Rohan <rohanakode52@gmail.com>
2023-10-20 00:45:13 +0530

    Error Handlers - duplicate key, json web token and expired token

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -7,7 +7,25 @@ module.exports = (err, req, res, next) => {
   // if wrong id / short id is given
   if (err.name === "CastError") {
     const message = `Resource not found.\n Invalid: ${err.path}`;
-    err = new ErrorHandler(message, 400)
+    err = new ErrorHandler(message, 400);
+  }
+
+  // mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+  
+  // wrong JWT Error   
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // JWT Expire error 
+  if (err.name === "TokenExpiredError") {
+    const message = `Json Web Token Expired, try again`;
+    err = new ErrorHandler(message, 400);
   }
 
   res.status(err.statusCode).json({
