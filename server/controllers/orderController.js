9f40bb1cccc10455c9696134364e1d32cfae86b1
Rohan <rohanakode52@gmail.com>
2023-10-21 22:26:07 +0530

    Order get single Orders and Logged in user orders

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -28,7 +28,35 @@ exports.newOrder = catchAsyncError(async (req, res, next) => {
   });
 
   res.status(201).json({
-    success:true,
-    order
-  })
+    success: true,
+    order,
+  });
+});
+
+// get Single Order
+exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
+  // in the "order" fill the user name and email instead of user.id as well
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+// get logged in User Order Details
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.status(200).json({
+    success: true,
+    orders,
+  });
 });
--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,9 +1,21 @@
 const express = require("express");
-const { newOrder } = require("../controllers/orderController");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+} = require("../controllers/orderController");
 const { isAuthenticatedUser, authorizedRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
+
+router
+  .route("/order/:id")
+  .get(isAuthenticatedUser, getSingleOrder);
+
+router
+  .route("/orders/me")
+  .get(isAuthenticatedUser, myOrders);
 
 module.exports = router;
