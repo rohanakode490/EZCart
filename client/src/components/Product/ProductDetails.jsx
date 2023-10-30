4b3e0a0ef90e64ca8b98349ae88fb73d8c460830
Rohan <rohanakode52@gmail.com>
2023-10-31 00:01:11 +0530

    Product Details Page Styling

--- client/src/components/Product/ProductDetails.jsx
+++ client/src/components/Product/ProductDetails.jsx
@@ -4,10 +4,11 @@ import './ProductDetails.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProductDetails } from '../../actions/productAction'
 import { useParams } from 'react-router-dom'
+import ReactStars from 'react-rating-stars-component'
 
-const ProductDetails = () => { 
+const ProductDetails = () => {
 
-    const {id}=useParams();
+    const { id } = useParams();
     const dispatch = useDispatch();
 
     const { product, error, loading } = useSelector(state => state.productDetails)
@@ -15,6 +16,17 @@ const ProductDetails = () => {
     useEffect(() => {
         dispatch(getProductDetails(id))
     }, [dispatch, id])
+
+    
+    // React rating options
+    const options = {
+        edit: false,
+        color: "rgba(20, 20, 20, 0.1)",
+        activeColor: "tomato",
+        size: window.innerWidth < 600 ? 20 : 25,
+        value: product.ratings,
+        isHalf: true
+    }
 
     return (
         <>
@@ -24,12 +36,51 @@ const ProductDetails = () => {
                     <Carousel>
                         {product.images &&
                             product.images.map((item, index) => (
-                                <img src={item.url} key={item.url} alt={`${index} Slide`} className="CarouselImage" />
+                                <img src={item.url} key={index} alt={`${index} Slide`} className="CarouselImage" />
                             ))
-
                         }
                     </Carousel>
                 </div>
+
+                <div>
+                    <div className="detailsBlock-1">
+                        <h2>{product.name}</h2>
+                        <p>Product $ {product._id}</p>
+                    </div>
+
+                    {/* Reviews  */}
+                    <div className="detailsBlock-2">
+                        <ReactStars {...options} />
+                        <span>({product.numOfReviews} Reviews)</span>
+                    </div>
+
+                    {/* Price*/}
+                    <div className="detailsBlock-3">
+                        <h1>{`\u20B9 ${product.price}`}</h1>
+                        {/* Cart */}
+                        <div className='detailsBlock-3-1'>
+                            <div className='detailsBlock-3-1-1'>
+                                <button>-</button>
+                                <input type="number" value="1" />
+                                <button>+</button>
+                            </div>{" "}
+                            <button>Add to Cart</button>
+                        </div>
+
+                        <p>
+                            Status:{" "}
+                            <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                                {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                            </b>
+                        </p> 
+                    </div>
+
+                    <div className="detailsBlock-4">
+                        Description: <p>{product.description}</p>
+                    </div>
+
+                    <button className='submitReview'>Submit Review</button>
+                 </div>
             </div>
         </>
     )
