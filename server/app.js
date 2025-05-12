e4eac8d9884a392b7a5129279f1ded3665b8a03e
Rohan Akode <rohanakode52@gmail.com>
2025-05-12 19:54:34 +0530

    Fixed CORS

--- server/app.js
+++ server/app.js
@@ -12,7 +12,12 @@ const rateLimit = require('express-rate-limit');
 dotenv.config();
 
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(rateLimit({
