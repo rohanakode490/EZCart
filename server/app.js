7c961d346ff6c319b2228199bc53763555b68428
Rohan Akode <rohanakode52@gmail.com>
2025-05-12 20:12:22 +0530

    Fixed Credentials

--- server/app.js
+++ server/app.js
@@ -13,7 +13,8 @@ dotenv.config();
 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173"
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
   }));
 app.use(express.json());
 app.use(cookieParser());
