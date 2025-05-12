d409795056fe14c74fcccc68605111baf9419533
Rohan Akode <rohanakode52@gmail.com>
2025-05-12 20:07:27 +0530

    Fixed CORS2

--- server/app.js
+++ server/app.js
@@ -13,10 +13,7 @@ dotenv.config();
 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: process.env.FRONTEND_URL || "http://localhost:5173"
   }));
 app.use(express.json());
 app.use(cookieParser());
